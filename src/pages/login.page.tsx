14de2e421d3849ad07ffee3fa231af5e4ee95558
Isaias <isaias.trujillo@unmsm.edu.pe>
2025-06-16 09:20:52 -0500

    remove external link

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -11,12 +11,7 @@ const LoginPage = () => {
       </section>
       <aside className="w-[clamp(min(20rem,100%),25rem+5dvw,calc(4/10*100%))] flex flex-wrap flex-col place-content-center place-items-center">
         <img src={aside} alt="A woman" />
-        <a
-          target="_blank"
-          href="https://www.freepik.com/free-vector/team-checklist-concept-illustration_29808405.htm#fromView=search&page=1&position=5&uuid=c501743a-afca-460a-bafc-5f39407ae441"
-        >
-          Image by storyset on Freepik
-        </a>
+        <span>Image by storyset on Freepik</span>
       </aside>
     </main>
   );
